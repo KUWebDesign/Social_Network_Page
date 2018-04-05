99e822e55310b3d8d14e74adca610f465bfa322b
Giovaartavia <giovaartavia@hotmail.com>
2018-04-05 13:56:57 -0500

    Added basic account creation modal to index.ejs
    Added basic account creation modal to index.ejs.
    Created js folder
    Created img folder

--- main.js
+++ main.js
@@ -2,10 +2,13 @@ var express = require("express");
 var app = express();
 
 app.use(express.static("css"));
-
+app.use(express.static("js"));
+app.use(express.static("img"));
 
 app.get("/", function(req, res){
     res.render("index.ejs");
 });
 
-app.listen("8080");
+app.listen("8080", function(){
+    console.log("Starting localhost:8080");
+});
