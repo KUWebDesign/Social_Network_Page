875b8c9386bf9767e32d30f7a3430698f1dac01f
Giovaartavia <giovaartavia@hotmail.com>
2018-04-12 19:00:19 -0500

    Added basic login check
    Added basic login check

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 var express = require("express");
+var bodyParser = require("body-parser");
+
 var app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static("css"));
 app.use(express.static("js"));
@@ -8,9 +11,21 @@ app.use(express.static("img"));
 app.get("/", function(req, res){
     res.render("index.ejs");
 });
-app.get("/home", function(req, res){
-	res.render("home.ejs");
-})
+
+app.post("/home", function(req, res){
+    let emailLogin = req.body.emailLogin;
+    let passwordLogin = req.body.passwordLogin;
+	res.render("home.ejs", {emailLogin: emailLogin, passwordLogin: passwordLogin});
+});
+
+app.get("/profile", function(req, res){
+    res.render("profile.ejs");
+});
+
+app.get("*", function(req, res){
+    res.render("notFound.ejs");
+});
+
 
 app.listen("8080", function(){
     console.log("Starting localhost:8080");
